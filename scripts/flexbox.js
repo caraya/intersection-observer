ca990544c666c9a44b22efb4eccef482a62f540e
Carlos Araya <carlos.araya@gmail.com>
2017-03-12 18:22:00 -0700

    added images

--- scripts/flexbox.js
+++ scripts/flexbox.js
@@ -1,20 +1,27 @@
+if (! 'IntersectionObserver' in window) {
+  console.log('Intersection Observers not supported');
+  function loadImages(changes) {
+    changes.forEach((change) => {
+      change.target.src = change.target.dataset.src;
+    })
+  }
+} else {
+  // 1. Create the IntersectionObserver and bind it to the function we want it to work with
+  let observer = new IntersectionObserver(onChange);
 
-// 1. Create the IntersectionObserver and bind it to the function we want it to work with
-let observer = new IntersectionObserver(onChange);
-
-function onChange(changes) {
-  // 2. For each image that we want to change
-  changes.forEach(change => {
-    // 3. take image url from `data-src` attribute
-    change.target.src = change.target.dataset.src;
-
+  function onChange(changes) {
+    // 2. For each image that we want to change
+    changes.forEach((change) => {
+      // 3. take image url from `data-src` attribute
+      change.target.src = change.target.dataset.src;
     // 4. Stop observing the current target
     observer.unobserve(change.target);
   })
-}
+  }
 
 // 5. Convert node list of all images with data-src attributed to array
-const imgs = [ ...document.querySelectorAll('img[data-src]') ];
+  const imgs = [ ...document.querySelectorAll('img[data-src]') ];
 
 // 6. Observe each image derived from the array above
-imgs.forEach(img => observer.observe(img));
+  imgs.forEach((img) => observer.observe(img));
+}
